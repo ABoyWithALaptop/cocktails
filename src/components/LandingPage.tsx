67ddd926304b641db7c72993138d35805a591f7b
ABoyWithALaptop <tupx1204@gmail.com>
2025-08-07 15:35:38 +0700

    Implement the About section

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -3,15 +3,17 @@
 import Hero from "./hero";
 import NavBar from "./navBar";
 import {Cocktails} from "@/src/components/cocktails";
+import About from "@/src/components/About"
 
 const LandingPage = () => {
-	return (
-		<main>
-			<NavBar />
-			<Hero />
-			<Cocktails />
-		</main>
-	);
+    return (
+        <main>
+            <NavBar/>
+            <Hero/>
+            <Cocktails/>
+            <About/>
+        </main>
+    );
 };
 
 export default LandingPage;
